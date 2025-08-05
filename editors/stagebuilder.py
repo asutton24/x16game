7b1f1f726bc688e7d23f552abc857d91d8464d8d
asutton24 <asutton24@outlook.com>
2025-08-05 13:08:14 -0400

    created stagebuilder

---
+++ editors/stagebuilder.py
@@ -0,0 +1,12 @@
+low = int(input("enter begining of stage"))
+length = int(input("enter length of stage"))
+id = int(input("enter stage id")) % 16
+stage = b''
+for i in range(low, low + length):
+    with open("Levels/LVL" + f"{i:02X}" + ".BIN", "rb") as file:
+        stage += file.read().ljust(512, b'\x00')
+        file.close()
+with open("STG" + f"{id:X}" + ".BIN", "wb") as file:
+    file.write(stage)
+    file.close()
+
